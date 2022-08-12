1839108cdc30cb09b492c9ae1b9cd4aca1a4e1c0
Noob169 <boney0310@gmail.com>
2022-08-13 01:14:35 +0530

    fixed merge conflicts with beta branch

--- app/src/main/java/com/example/Maatran/ReportsActivity.java
+++ app/src/main/java/com/example/Maatran/ReportsActivity.java
@@ -1,12 +1,5 @@
 package com.example.Maatran;
 
-<<<<<<<< HEAD:app/src/main/java/com/example/Maatran/SensorsHome.java
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SensorsHome extends AppCompatActivity {
-========
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,7 +7,6 @@ import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ReportsActivity extends AppCompatActivity {
->>>>>>>> beta:app/src/main/java/com/example/Maatran/ReportsActivity.java
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
