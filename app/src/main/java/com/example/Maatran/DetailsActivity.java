564ef938e368c80f0e69428aa25e9329403cb0c7
kulsum kamal <kulsumkamal26@gmail.com>
2022-08-18 21:27:52 +0530

    ignore

--- app/src/main/java/com/example/Maatran/DetailsActivity.java
+++ app/src/main/java/com/example/Maatran/DetailsActivity.java
@@ -11,7 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 //Quiz Activity
-//called after EmailSignUp is complete
+//called every time user needs to add/edit details
+//receives a Bundle containing Boolean value from the intent calling it...
+//..which specifies whether the user is adding or editing details of a user or of a patient
 //xml file: screen_4
 public class DetailsActivity extends AppCompatActivity {
 
@@ -33,17 +35,22 @@ public class DetailsActivity extends AppCompatActivity {
         save.setOnClickListener(view -> {
             String name = ((EditText) findViewById(R.id.name)).getText().toString();
             String gender = ((EditText) findViewById(R.id.gender)).getText().toString();
-            String mobile = ((EditText) findViewById(R.id.mobile_number)).getText().toString();
-            String emergency_mobile = ((EditText) findViewById(R.id.emergency_no)).getText().toString();
+            int mobile = Integer.parseInt(((EditText) findViewById(R.id.mobile_number)).getText().toString());
+            int emergency_mobile = Integer.parseInt(((EditText) findViewById(R.id.emergency_no)).getText().toString());
             String address = ((EditText) findViewById(R.id.address)).getText().toString();
             long age = Long.parseLong(((EditText) findViewById(R.id.age)).getText().toString());
+            boolean isPatient = getIntent().getExtras().getBoolean("isPatient");        //get boolean value from intent
+            //if user is adding details of a patient, isPatient is true
+            //if user is adding details of a user, isPatient is false
+
             boolean flag=true;
-            if(mobile.length()!=10)
+            //check if all fields are filled and if all fields are valid
+            if(Integer.toString(mobile).length()!=10)
             {
                 Toast.makeText(getApplicationContext(),"Enter the correct mobile number",Toast.LENGTH_SHORT).show();
                 flag=false;
             }
-            if(emergency_mobile.length()!=10)
+            if(Integer.toString(emergency_mobile).length()!=10)
             {
                 Toast.makeText(getApplicationContext(), "Enter the correct emergency mobile number", Toast.LENGTH_SHORT).show();
                 flag=false;
@@ -53,11 +60,11 @@ public class DetailsActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Only users above or 18 and below 90 are allowed to register", Toast.LENGTH_SHORT).show();
                 flag=false;
             }
-            if(flag==true)
+            if(flag)
             {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                FirebaseHandler fdb = new FirebaseHandler(name, gender, address, mobile, emergency_mobile, age);
+                FirebaseHandler fdb = new FirebaseHandler(name, gender, address, Integer.toString(mobile), Integer.toString(emergency_mobile), age, isPatient);
                 fdb.addDetails(user);
 
                 Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
--- app/src/main/java/com/example/Maatran/ProfileView.java
+++ app/src/main/java/com/example/Maatran/ProfileView.java
@@ -1,10 +1,17 @@
 package com.example.Maatran;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,13 +24,20 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class ProfileView extends AppCompatActivity {
     FirebaseFirestore db;
     public static final String TAG="ProfileView";
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
         getSupportActionBar().hide();
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching data..");
+        progressDialog.show();
         FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
         getUserDetails(user);
-        setContentView(R.layout.user_profile);
+
     }
 
 
@@ -47,26 +61,102 @@ public class ProfileView extends AppCompatActivity {
                     tv_adr.setText(document.getData().get("address").toString());
                     TextView tv_mob= findViewById(R.id.user_mob);
                     tv_mob.setText(document.getData().get("mobile").toString());
-                    Log.d(TAG, "DocumentSnapshot data: " + document.getData().get("address"));
-                } else {
+
+                }
+                else
+                {
                     Log.d(TAG, "No such document");
                 }
-            } else {
+            }
+            else
+            {
                 Log.d(TAG, "get failed with ", task.getException());
             }
+            if(progressDialog.isShowing())
+                progressDialog.dismiss();
         });
     }
 
     public void editProfile(View view)
     {
         Intent intent = new Intent(getApplicationContext(),DetailsActivity.class);
+        intent.putExtra("isPatient",false);
         startActivity(intent);
     }
+
     public void signOut(View view)
     {
         FirebaseAuth mAuth = FirebaseAuth.getInstance();
         mAuth.signOut();
         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
         startActivity(intent);
+    }
+
+    public void deleteProfile(View view)
+    {
+        LayoutInflater inflater = getLayoutInflater();
+        View popupDeleteProfile = inflater.inflate(R.layout.popupview_confirmation,null);
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        // lets taps outside the popupWindow dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupDeleteProfile, width, height, true);
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+        Button yes = popupDeleteProfile.findViewById(R.id.btn_yes);
+        Button no = popupDeleteProfile.findViewById(R.id.btn_no);
+        yes.setOnClickListener(v -> {
+            ProgressDialog progressDialog = new ProgressDialog(ProfileView.this);
+            progressDialog.setCancelable(false);
+            progressDialog.setMessage("Deleting profile..");
+            progressDialog.show();
+            deleteUserProfile(progressDialog,popupWindow);
+        });
+        no.setOnClickListener(v -> popupWindow.dismiss());
+    }
+
+    public void backToDashboard(View view)
+    {
+        super.finish();
+    }
+
+    public void changePassword(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(),ChangePasswordActivity.class);
+        intent.putExtra("UserName",((TextView) findViewById(R.id.user_name)).getText().toString());
+        startActivity(intent);
+    }
+
+    public void deleteUserProfile(ProgressDialog progressDialog,PopupWindow popupWindow)
+    {
+        FirebaseUser user=FirebaseAuth.getInstance().getCurrentUser();
+        user.delete().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                deleteUserData(user);
+                Log.d(TAG, "User account deleted.");
+                Toast toast = Toast.makeText(getApplicationContext(), "User account deleted.", Toast.LENGTH_SHORT);
+                toast.show();
+                progressDialog.dismiss();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            } else {
+                Toast toast = Toast.makeText(getApplicationContext(), "Error deleting user account.", Toast.LENGTH_SHORT);
+                toast.show();
+                progressDialog.dismiss();
+                popupWindow.dismiss();
+                Log.d(TAG, "Error deleting document", task.getException());
+            }
+        });
+    }
+
+    public void deleteUserData(FirebaseUser user)
+    {
+        db=FirebaseFirestore.getInstance();
+        DocumentReference docRef=db.collection("UserDetails").document(user.getEmail());
+        docRef.delete().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Log.d(TAG, "DocumentSnapshot successfully deleted!");
+            } else {
+                Log.d(TAG, "Error deleting document", task.getException());
+            }
+        });
     }
 }
