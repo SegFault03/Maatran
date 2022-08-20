fe4ffb033e6744e1f066e51b2a0b48576be99304
Niladri Das <boney0310@gmail.com>
2022-08-20 13:55:00 +0530

    RecyclerView is now rendered in real time

--- app/src/main/java/com/example/Maatran/PatientsView.java
+++ app/src/main/java/com/example/Maatran/PatientsView.java
@@ -9,16 +9,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-
+//Activity that initializes the RecyclerView
+//The interface onPatientListener is defined in UserAdapter.class
 public class PatientsView extends AppCompatActivity implements UserAdapter.OnPatientListener {
     RecyclerView recyclerView;
     ArrayList<User> userArrayList;
@@ -26,6 +25,7 @@ public class PatientsView extends AppCompatActivity implements UserAdapter.OnPat
     UserAdapter userAdapter;
     FirebaseFirestore db;
     ProgressDialog progressDialog;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,39 +38,38 @@ public class PatientsView extends AppCompatActivity implements UserAdapter.OnPat
         progressDialog.setMessage("Fetching data..");
         progressDialog.show();
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
+        user = FirebaseAuth.getInstance().getCurrentUser();
         recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         userArrayList = new ArrayList<>();
-        userAdapter = new UserAdapter(PatientsView.this, userArrayList, this);
+        userAdapter = new UserAdapter(PatientsView.this, userArrayList, this);      //Creates a new Adapter with current Activity as context
         recyclerView.setAdapter(userAdapter);
-
         db = FirebaseFirestore.getInstance();
         userId = new ArrayList<>();
-
-        EventChangeListener(user);
-
-
     }
 
-    private void EventChangeListener(FirebaseUser user) {
+    //For re-rendering the RecyclerView when some change is made (Patient Data is Edited/Deleted)
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        EventChangeListener();
+    }
 
+    private void EventChangeListener() {
+
+        userArrayList.clear();                                  //clears the existing PatientDetails and fills it up with new and updated data
         CollectionReference ref = db.collection("UserDetails").document(user.getEmail()).collection("Patients");
-        ref.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-            @Override
-            public void onSuccess(QuerySnapshot value) {
-                for(DocumentSnapshot dc : value.getDocuments())
-                {
-                    userArrayList.add(dc.toObject(User.class));
-                    userId.add(dc.getId());
-                }
-                userAdapter.notifyDataSetChanged();
-                if (progressDialog.isShowing())
-                    progressDialog.dismiss();
+        ref.get().addOnSuccessListener(value -> {
+            for(DocumentSnapshot dc : value.getDocuments())
+            {
+                userArrayList.add(dc.toObject(User.class));     //Converting the DocuSnapshot to a User.class object
+                userId.add(dc.getId());
             }
+            userAdapter.notifyDataSetChanged();
+            if (progressDialog.isShowing())
+                progressDialog.dismiss();
         });
     }
 
@@ -79,10 +78,11 @@ public class PatientsView extends AppCompatActivity implements UserAdapter.OnPat
         super.finish();
     }
 
+    //defines the onPatientClick method of the interface: onPatientListener
     @Override
     public void onPatientClick(int position) {
         Intent intent = new Intent(getApplicationContext(), ReportsActivity.class);
-        intent.putExtra("user", userArrayList.get(position));
+        intent.putExtra("user", userArrayList.get(position));   //passes a User object by encoding it in a Parcel
         intent.putExtra("id", userId.get(position));
         startActivity(intent);
     }
