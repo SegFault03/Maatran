e6f712540e4941901911fcd7b93cd44d46cfdadd
Niladri Das <boney0310@gmail.com>
2022-08-27 21:59:21 +0530

    added code to turn bluetooth on/off

--- app/src/main/java/com/example/Maatran/BluetoothActivity.java
+++ app/src/main/java/com/example/Maatran/BluetoothActivity.java
@@ -1,15 +1,11 @@
 package com.example.Maatran;
 
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothManager;
-import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,26 +13,34 @@ import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 public class BluetoothActivity extends AppCompatActivity {
 
-    private Button mBluetoothState;
+    //private data members corresponding to views in the layout
+    private Button mBluetoothStateChangeBtn;
     private ListView mBluetoothDeviceList;
     private TextView mSelectDeviceText;
+    private TextView mBluetoothStateText;
     ProgressDialog progressDialog;
+
+    //Handler for updating UI after a specific time interval
+    private Handler handler;
+
     private static final String TAG = "BluetoothActivity";
 
     //CODES TO INDICATE BLUETOOTH STATUS
     private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
     private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
     private static final int REQUEST_ENABLE_BT = 3;
+
     //Name of connected device
     private String mConnectedDeviceName = null;
+
     //Global BluetoothAdapter class object to use Bluetooth features
     private BluetoothAdapter mBluetoothAdapter = null;
+
     //Global BluetoothService object
-    private BluetoothService mChatService = null;                   //TODO
+    //private BluetoothService mChatService = null;                   //TODO
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -47,10 +51,15 @@ public class BluetoothActivity extends AppCompatActivity {
         //Initializing data members
         progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
-        mBluetoothState = findViewById(R.id.bluetooth_service_connect_btn);
+        mBluetoothStateChangeBtn = findViewById(R.id.bluetooth_service_connect_btn);
         mBluetoothDeviceList = findViewById(R.id.device_list_lv);
         mSelectDeviceText = findViewById(R.id.select_device_text);
         mSelectDeviceText.setVisibility(View.INVISIBLE);
+        mBluetoothStateText=findViewById(R.id.bluetooth_service_state_text);
+
+        //Creating a new handler and binding it with a callback fn: runnable
+        handler=new Handler();
+        handler.postDelayed(runnable, 100);
 
         //Setting up bluetooth
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -62,52 +71,72 @@ public class BluetoothActivity extends AppCompatActivity {
         }
 
         //Setting an onClickListener to start Bluetooth
-        mBluetoothState.setOnClickListener(v -> setUpBluetooth());
+
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void setUpBluetooth() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateUI();
+    }
 
-        //Asking for user permission to turn on Bluetooth
-        if (!mBluetoothAdapter.isEnabled()) {
-            Toast.makeText(this, "Please turn on Bluetooth to proceed", Toast.LENGTH_SHORT).show();
-            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+    //Runnable for the handler
+    private final Runnable runnable = new Runnable()
+    {
+        @Override
+        public void run() {
+            //Update UI
+            updateUI();
+            //call it again after 100ms
+            handler.postDelayed(this, 100);
         }
+    };
 
-        //Makes this device discoverable for 300 seconds (5 minutes).
-        progressDialog.setMessage("Setting up Bluetooth, please wait...");
-        progressDialog.show();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+    //Update UI according to Bluetooth State
+    public void updateUI()
+    {
+        final String[] BLUETOOTH_STATE={"BLUETOOTH OFF","BLUETOOTH TURNING ON","BLUETOOTH ON","BLUETOOTH TURNING OFF"};
+        switch (mBluetoothAdapter.getState())
+        {
+            case BluetoothAdapter.STATE_ON:
+                mBluetoothStateText.setText(BLUETOOTH_STATE[2]);
+                mBluetoothStateChangeBtn.setText("TURN "+BLUETOOTH_STATE[0]);
+                mBluetoothStateChangeBtn.setOnClickListener(view->
+                    mBluetoothAdapter.disable()
+                );
+                break;
+            case BluetoothAdapter.STATE_OFF:
+                mBluetoothStateText.setText(BLUETOOTH_STATE[0]);
+                mBluetoothStateChangeBtn.setText("TURN "+BLUETOOTH_STATE[2]);
+                mBluetoothStateChangeBtn.setOnClickListener(view->
+                        mBluetoothAdapter.enable()
+                );
+                break;
+            case BluetoothAdapter.STATE_TURNING_OFF:
+                mBluetoothStateText.setText(BLUETOOTH_STATE[3]);
+                mBluetoothStateChangeBtn.setText(BLUETOOTH_STATE[3]);
+                mBluetoothStateChangeBtn.setOnClickListener(view->
+                        Toast.makeText(this,"not allowed",Toast.LENGTH_SHORT).show()
+                );
+                break;
+            case BluetoothAdapter.STATE_TURNING_ON:
+                mBluetoothStateText.setText(BLUETOOTH_STATE[1]);
+                mBluetoothStateChangeBtn.setText(BLUETOOTH_STATE[1]);
+                mBluetoothStateChangeBtn.setOnClickListener(view->
+                        Toast.makeText(this,"not allowed",Toast.LENGTH_SHORT).show()
+                );
+                break;
         }
-        if (mBluetoothAdapter.getScanMode() !=
-                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADVERTISE) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            startActivity(discoverableIntent);
-        }
+    }
+
+
+
+    /*public void setUpBluetooth() {
+
 
         mSelectDeviceText.setVisibility(View.VISIBLE);
         String[] array = {"Niladri","Rahul","Abhishek"};
         ArrayAdapter<String> mListOfDevices = new ArrayAdapter<>(this, R.layout.listview_elements,R.id.device_list_item_lv, array);
         mBluetoothDeviceList.setAdapter(mListOfDevices);
-    }
+    }*/
 }
