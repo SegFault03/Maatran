4d6f668e895517fd28f77fcee3d5a8dbb48aa7b6
Niladri Das <boney0310@gmail.com>
2022-09-06 01:14:09 +0530

    QOL changes: added a 'TEST' btn in mainActivity that opens up dashboard with a pre-existing account for testing/debugging purposes

--- app/src/main/java/com/example/Maatran/MainActivity.java
+++ app/src/main/java/com/example/Maatran/MainActivity.java
@@ -33,6 +33,8 @@ ProgressDialog progressDialog;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_1);
         progressDialog=new ProgressDialog(this);
+        Button test_btn = findViewById(R.id.test_btn);
+        test_btn.setOnClickListener(v-> signInWithTestAccount());
     }
 
     //for signing-in, calls LoginActivity.class
@@ -107,6 +109,28 @@ ProgressDialog progressDialog;
             else
                 Log.d(TAG,"Task failed to complete");
         });
+    }
+
+    private void signInWithTestAccount()
+    {
+        String email = "testpatient4@gmail.com";
+        String password = "123456";
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success");
+                        Toast.makeText(this,"WARNING: USE THIS FEATURE FOR DEBUG/TEST PURPOSES ONLY",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(getApplicationContext(), DashboardActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(this, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
 
