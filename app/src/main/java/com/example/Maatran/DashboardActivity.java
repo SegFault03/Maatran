9565880e7c875d02d47acacd2354ee0a215d8bca
kulsum kamal <kulsumkamal26@gmail.com>
2022-12-30 20:08:46 +0530

    added sms sos feature to all patients in one account

--- app/src/main/java/com/example/Maatran/DashboardActivity.java
+++ app/src/main/java/com/example/Maatran/DashboardActivity.java
@@ -1,16 +1,24 @@
 package com.example.Maatran;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -22,11 +30,17 @@ public class DashboardActivity extends AppCompatActivity {
     ProgressDialog progressDialog;
     FirebaseUser user;
     boolean isPatient = true;
+    private static final int PERMISSION_SEND_SMS = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_1);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED)
+        {
+            Toast.makeText(this,"Permission ti send messages not available! Please grant it to continue!",Toast.LENGTH_SHORT).show();
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, PERMISSION_SEND_SMS);
+        }
 
         progressDialog = new ProgressDialog(this);
         progressDialog.setCancelable(false);
@@ -86,6 +100,43 @@ public class DashboardActivity extends AppCompatActivity {
                 Log.d(TAG, "get failed with ", task.getException());
             if(progressDialog.isShowing())
                 progressDialog.dismiss();
+        });
+    }
+
+    public void sendSOS(View View)
+    {
+        FirebaseFirestore db=FirebaseFirestore.getInstance();
+        DocumentReference df= db.collection("UserDetails").document(Objects.requireNonNull(user.getEmail()));
+        CollectionReference ref = df.collection("Patients");
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,PendingIntent.FLAG_IMMUTABLE);
+        SmsManager sms=SmsManager.getDefault();
+        df.get().addOnCompleteListener(task -> {
+            if(task.isSuccessful())
+            {
+                DocumentSnapshot ds = task.getResult();
+                if(ds.exists()) {
+                    String number = Objects.requireNonNull(ds.get("mobile")).toString();
+                    //Getting intent and PendingIntent instance
+
+
+                    //Get the SmsManager instance and call the sendTextMessage method to send message
+                    sms.sendTextMessage(number, null, "I need help!", pi,null);
+
+                }
+                else
+                    Log.d(TAG, "No such document");
+            }
+            else
+                Log.d(TAG, "get failed with ", task.getException());
+            if(progressDialog.isShowing())
+                progressDialog.dismiss();
+        });
+        ref.get().addOnSuccessListener(value -> {
+            for (DocumentSnapshot dc : value.getDocuments()) {
+                String number = Objects.requireNonNull(dc.get("mobile")).toString();
+                sms.sendTextMessage(number, null, "I need help!", pi,null);
+            }
         });
     }
 
